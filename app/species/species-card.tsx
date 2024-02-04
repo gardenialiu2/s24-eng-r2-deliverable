44197e804dee4f8d0cf08d5fbdc7ac1a05fb27ba
gardenia liu <gardenialiu5@gmail.com>
2024-02-04 16:18:57 -0500

    species list, add new species

--- app/species/species-card.tsx
+++ app/species/species-card.tsx
@@ -13,6 +13,7 @@ can cause errors with matching props and state in child components if the list o
 import { Button } from "@/components/ui/button";
 import type { Database } from "@/lib/schema";
 import Image from "next/image";
+import SpeciesDetailsDialog from "./species-details-dialog";
 type Species = Database["public"]["Tables"]["species"]["Row"];
 
 export default function SpeciesCard({ species }: { species: Species }) {
@@ -28,6 +29,7 @@ export default function SpeciesCard({ species }: { species: Species }) {
       <p>{species.description ? species.description.slice(0, 150).trim() + "..." : ""}</p>
       {/* Replace the button with the detailed view dialog. */}
       <Button className="mt-3 w-full">Learn More</Button>
+      <SpeciesDetailsDialog species={species} />
     </div>
   );
 }
